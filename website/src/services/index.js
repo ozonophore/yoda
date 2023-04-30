ddcf2d38cd558bbfb329eaf22e928eb37bc2aa1b
Aleksey Zverev <ozonophore@gmail.com>
2023-04-30 23:16:30 +0300

    Delete conso;e.log

--- website/src/services/index.js
+++ website/src/services/index.js
@@ -2,7 +2,6 @@ import { parseISO } from "date-fns";
 import { NextRun } from "../context/actions";
 
 function ping(ws) {
-  console.log("#Ping");
   ws.send("__ping__");
 }
 
@@ -10,16 +9,13 @@ let ws = null;
 
 const createWSService = (dispatch) => {
   if (ws) {
-    console.log("#Already Connected");
     return;
   }
-  console.log("#New Connecting");
   let interval = null;
   const host = window.location.hostname.match("([\\w\\.]+)*(:[0-9]+)?")[1];
   ws = new WebSocket(`ws://${host}:88/ws`);
 
   ws.onopen = () => {
-    console.log("#Connected");
     interval = setInterval(() => {
       ping(ws);
     }, 30000);
@@ -27,16 +23,13 @@ const createWSService = (dispatch) => {
 
   ws.onmessage = (evt) => {
     if (evt.data === "__pong__") {
-      console.log("#Pong");
       return;
     }
-    console.log("#Received: ", evt.data);
     const message = JSON.parse(evt.data);
     dispatch(NextRun(parseISO(message.nextRun)));
   };
 
   ws.onclose = () => {
-    console.log("#Disconnected");
     clearInterval(interval);
   };
 };
