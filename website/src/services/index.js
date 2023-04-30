fe554d09067f4357de941d8ce935724f51a73e8e
Aleksey Zverev <ozonophore@gmail.com>
2023-04-30 22:03:13 +0300

    Fix loglevel

--- website/src/services/index.js
+++ website/src/services/index.js
@@ -7,10 +7,18 @@ const createWSService = (dispatch) => {
   console.log("#host: ", host);
   const ws = new WebSocket(`ws://${host}:88/ws`);
 
+  ws.onopen = () => {
+    console.log("#Connected");
+  };
+
   ws.onmessage = (evt) => {
     console.log("#Received: ", evt.data);
     const message = JSON.parse(evt.data);
     dispatch(NextRun(parseISO(message.nextRun)));
+  };
+
+  ws.onclose = () => {
+    console.log("#Disconnected");
   };
 };
 
