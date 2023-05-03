5779e662bb10c3cebd3b87ee27c9cbb4cf681854
Aleksey Zverev <ozonophore@gmail.com>
2023-05-03 10:11:01 +0300

    Fix test

--- server/app/internal/integration/client_test.go
+++ server/app/internal/integration/client_test.go
@@ -8,20 +8,11 @@ import (
 	"testing"
 )
 
-const apiURL = "http://api.github.com"
-
 var (
 	mux    *http.ServeMux
 	server *httptest.Server
 	client integration.ClientWithResponsesInterface
 )
-
-func BaseURL(baseURL string) integration.ClientOption {
-	return func(c *integration.Client) error {
-		c.Server = baseURL
-		return nil
-	}
-}
 
 func setup() func() {
 	mux = http.NewServeMux()
@@ -41,20 +32,23 @@ func TestGetOrganization(t *testing.T) {
 	mux.HandleFunc("/organizations", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`[{
-            "id": "f6a89b8b-964d-11ed-852f-7c10c9211780",
-            "name": "org1",
-            "inn": "",
-            "kpp": "",
-            "updateAt": "2023-04-27T09:50:33"
-        },
-        {
-            "id": "ecf9c10e-9bdf-11ed-852f-7c10c9211780",
-            "name": "org2",
-            "inn": "1111111111",
-            "kpp": "",
-            "updateAt": "2023-04-27T16:14:36"
-        }]`))
+		w.Write([]byte(`{
+					"count": 2,
+					"items": [
+						{
+							"id": "f6a89b8b-964d-11ed-852f-7c10c9211780",
+							"name": "org1",
+							"inn": "",
+							"kpp": "",
+							"updateAt": "2023-04-27T09:50:33"
+						},
+						{
+							"id": "ecf9c10e-9bdf-11ed-852f-7c10c9211780",
+							"name": "org2",
+							"inn": "1111111111",
+							"kpp": "",
+							"updateAt": "2023-04-27T16:14:36"
+        }]}`))
 	})
 
 	resp, err := client.GetOrganizationsWithResponse(context.Background())
@@ -65,15 +59,15 @@ func TestGetOrganization(t *testing.T) {
 	if resp.StatusCode() != http.StatusOK {
 		t.Errorf("expected status code %d, got %d", http.StatusOK, resp.StatusCode())
 	}
-	items := resp.JSON200
-	if items == nil {
+	result := resp.JSON200
+	if result == nil {
 		t.Fatal("expected items, got nil")
 	}
-	if len(*items) != 2 {
-		t.Errorf("expected 2 organizations, got %d", len(*items))
+	if result.Count != 2 {
+		t.Errorf("expected 2 organizations, got %d", result.Count)
 	}
 
-	item := (*items)[0]
+	item := result.Items[0]
 	if item.Id != "f6a89b8b-964d-11ed-852f-7c10c9211780" {
 		t.Errorf("expected id 1, got %s", item.Id)
 	}
