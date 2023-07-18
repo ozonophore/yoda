08215a90abd7791f794bef71085555cc065270c4
Aleksey Zverev <ozonophore@gmail.com>
2023-07-18 09:50:22 +0300

    Org name

--- web/internal/storage/stock.go
+++ web/internal/storage/stock.go
@@ -15,10 +15,12 @@ func (s *Storage) GetStocksByDate(date time.Time) (*[]Stock, error) {
 	stocks := []Stock{}
 	err := s.db.Raw(`with t as (select owner_code, source, barcode, round(sum(quantity)) quantity from dl.stock_daily
                                                               where stock_date = ?
-                                                              group by owner_code, source, barcode)
-			select owner_code, o.organisation_id, t.source, m.marketplace_id, barcode, quantity
+                                                              group by owner_code, source, barcode
+															  having sum(quantity) > 0)
+			select o.name owner_code, o.organisation_id, t.source, m.marketplace_id, barcode, quantity
 			from t
 			inner join ml.marketplace m on m.code = t.source
+			inner join ml.owner o on o.code = t.owner_code
 			inner join ml.owner o on o.code = t.owner_code`, date.Format(time.DateOnly)).Scan(&stocks).Error
 	if err != nil {
 		return nil, err
